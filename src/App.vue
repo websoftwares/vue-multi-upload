8248ca024f194698637d2581569172df074cb96a
Boris <boris@websoftwar.es>
2017-11-14 07:59:27 +0100

    created grid view

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <DocumentsList/>
+  </div>
+</template>
+
+<script>
+import DocumentsList from './components/DocumentsList'
+
+export default {
+  name: 'app',
+  components: {
+    DocumentsList
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/DocumentsList.vue
@@ -0,0 +1,39 @@
+<template>
+<table>
+  <th><td>ID</td></th>
+  <th><td>Document</td></th>
+  <tr v-for="document of documents" v-if="documents && documents.length">
+    <td >{{document.id}}</td>
+    <td >{{document.filename}}</td>
+  </tr>
+  <tr v-for="error of errors" v-if="errors && errors.length">
+    <td >{{error}}</td>
+  </tr>
+</table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      documents: [],
+      errors: []
+    }
+  },
+
+  async created () {
+    try {
+      const response = await axios.get(`http://localhost:8080`)
+      this.documents = response.data
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
