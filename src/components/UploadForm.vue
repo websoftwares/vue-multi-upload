d923840f2e9ca4a9127823e1223c5d1fe801e963
Boris <boris@websoftwar.es>
2017-11-14 10:12:22 +0100

    Added button for upload file

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="upload" ref="upload" id="upload" enctype="multipart/form-data">
+    <input type="file" name="files[]" multiple><br />
+    <input type="submit">
+</form>
+</template>
+
+<script>
+  export default {
+    mounted () {
+      let form = this.$refs.upload
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let formdata = new FormData(form)
+
+        try {
+          await fetch('http://localhost:8080/upload.php', {method: 'POST', body: formdata})
+          window.location.reload(false)
+        } catch (error) {
+          console.error(error)
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped>
+</style>
